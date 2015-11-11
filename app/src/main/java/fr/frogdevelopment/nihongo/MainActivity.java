61bfd481f774659ee2f46909f844bbe1b8c80db1
Le Gall Benoit <legall.benoit@gmail.com>
2015-11-11 21:42:17 +0100

    update version

--- app/src/main/java/fr/frogdevelopment/nihongo/MainActivity.java
+++ app/src/main/java/fr/frogdevelopment/nihongo/MainActivity.java
@@ -7,50 +7,44 @@ package fr.frogdevelopment.nihongo;
 import android.app.AlertDialog;
 import android.app.SearchManager;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
 import android.view.inputmethod.InputMethodInfo;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.RelativeLayout;
 
 import java.util.List;
 import java.util.Locale;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
-import butterknife.InjectView;
 import fr.frogdevelopment.nihongo.data.Type;
 import fr.frogdevelopment.nihongo.dialog.WarningIMEDialog;
 import fr.frogdevelopment.nihongo.dico.DicoFragment;
-import fr.frogdevelopment.nihongo.drawer.DrawerItemAdapter;
 import fr.frogdevelopment.nihongo.help.HelpKanaFragment;
 import fr.frogdevelopment.nihongo.lessons.LessonsFragment;
 import fr.frogdevelopment.nihongo.options.ParametersFragment;
 import fr.frogdevelopment.nihongo.review.ReviewParametersFragment;
 import fr.frogdevelopment.nihongo.test.TestParametersFragment;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends AppCompatActivity {
 
-//    private static final String LOG_TAG = "NIHON_GO";
+	@Bind(R.id.drawer_layout)
+	DrawerLayout mDrawerLayout;
 
-	@InjectView(R.id.drawer_layout)
-	DrawerLayout   mDrawerLayout;
-	@InjectView(R.id.left_drawer)
-	RelativeLayout mDrawerRelativeLayout;
-	@InjectView(R.id.list_view_drawer)
-	ListView       mDrawerList;
+	@Bind(R.id.toolbar)
+	Toolbar toolbar;
 
 	private ActionBarDrawerToggle mDrawerToggle;
 
@@ -62,31 +56,45 @@ public class MainActivity extends FragmentActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
-
-		setContentView(R.layout.drawer);
-
+		ButterKnife.bind(this);
 		mDrawerTitle = getTitle();
-		ButterKnife.inject(this);
 
-		// set a custom shadow that overlays the main content when the drawer opens
-		mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
-		// set up the drawer's list view with items and click listener
+		initIME();
+		initToolbar();
+		setupDrawerLayout();
 
-		createMenuItemAdapter();
-
-		imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-
-		// enable ActionBar app icon to behave as action to toggle nav drawer
-		if (getActionBar() != null) {
-			getActionBar().setDisplayHomeAsUpEnabled(true);
-			getActionBar().setHomeButtonEnabled(true);
+		if (savedInstanceState == null) {
+			selectItemAtIndex(R.id.navigation_word, true);
 		}
 
-		// ActionBarDrawerToggle ties together the the proper interactions
-		// between the sliding drawer and the action bar app icon
-		mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
+		handleIntent(getIntent());
+	}
+
+	private void initToolbar() {
+		setSupportActionBar(toolbar);
+		final ActionBar actionBar = getSupportActionBar();
+
+		if (actionBar != null) {
+//			actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+			actionBar.setDisplayHomeAsUpEnabled(true);
+			actionBar.setHomeButtonEnabled(true);
+		}
+	}
+
+	private void setupDrawerLayout() {
+		NavigationView view = (NavigationView) findViewById(R.id.navigation_view);
+		view.setNavigationItemSelectedListener(menuItem -> {
+			menuItem.setChecked(true);
+			mDrawerLayout.closeDrawers();
+
+			selectItemAtIndex(menuItem.getItemId(), true);
+
+			return true;
+		});
+
+		mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
 			@Override
 			public void onDrawerClosed(View view) {
 				setTitle(mFragmentTitle);
@@ -101,17 +109,11 @@ public class MainActivity extends FragmentActivity {
 			}
 		};
 		mDrawerLayout.setDrawerListener(mDrawerToggle);
-
-		if (savedInstanceState == null) {
-			selectItemAtIndex(INDEX_DICO_WORD, true);
-		}
-
-		handleIntent(getIntent());
-
-		checkIME();
 	}
 
-	private void checkIME() {
+	private void initIME() {
+		imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+
 		boolean isNoJapanIME = true;
 		List<InputMethodInfo> inputMethodInfos = imm.getInputMethodList();
 		for (InputMethodInfo inputMethodInfo : inputMethodInfos) {
@@ -128,39 +130,16 @@ public class MainActivity extends FragmentActivity {
 			WarningIMEDialog.newInstance().show(getFragmentManager(), "warningIMEDialog");
 	}
 
-	private void createMenuItemAdapter() {
-		mFragmentTitle = R.string.menu_subitem_word;
-
-		final DrawerItemAdapter mAdapter = new DrawerItemAdapter(this);
-		mAdapter.addItem(R.string.menu_subitem_word, INDEX_DICO_WORD);
-		mAdapter.addItem(R.string.menu_subitem_expression, INDEX_DICO_EXPRESSION);
-		mAdapter.addItem(R.string.menu_subitem_kana, INDEX_KANA);
-
-		mAdapter.addSeparator();
-		mAdapter.addItem(R.string.menu_subitem_review, INDEX_REVIEW);
-		mAdapter.addItem(R.string.menu_subitem_test, INDEX_TEST);
-
-		mAdapter.addSeparator();
-		mAdapter.addItem(R.string.menu_subitem_lessons, INDEX_LESSONS);
-		mAdapter.addItem(R.string.menu_subitem_parameters, INDEX_PARAMETERS);
-
-		mDrawerList.setAdapter(mAdapter);
-		mDrawerList.setOnItemClickListener(new ListView.OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				view.setSelected(true);
-//                view.setActivated(true);
-				mAdapter.notifyDataSetChanged();
-				selectItemAtIndex(mAdapter.getIndex(position), true);
-			}
-		});
-	}
-
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
+
 			case R.id.menu_search:
 				// onSearchRequested();
+				return true;
+
+			case android.R.id.home:
+				mDrawerLayout.openDrawer(GravityCompat.START);
 				return true;
 
 			default:
@@ -176,27 +155,17 @@ public class MainActivity extends FragmentActivity {
 
 	private static int CURRENT_INDEX = -1;
 
-	static final int INDEX_DICO_WORD       = 1;
-	static final int INDEX_DICO_EXPRESSION = 2;
-	static final int INDEX_KANA            = 3;
-
-	static final int INDEX_REVIEW = 4;
-	static final int INDEX_TEST   = 5;
-
-	static final int INDEX_LESSONS    = 6;
-	static final int INDEX_PARAMETERS = 7;
-
-	private void selectItemAtIndex(int index, boolean invalideOptionsMenu) {
-		if (CURRENT_INDEX == index) {
+	private void selectItemAtIndex(int id, boolean invalideOptionsMenu) {
+		if (CURRENT_INDEX == id) {
 			return;
 		}
 
 		// Create a new fragment and specify the view to show based on index
 		Bundle args;
 		Fragment fragment;
-		switch (index) {
+		switch (id) {
 
-			case INDEX_DICO_WORD:
+			case R.id.navigation_word:
 				mFragmentTitle = R.string.menu_subitem_word;
 				fragment = new DicoFragment();
 				args = new Bundle();
@@ -204,7 +173,7 @@ public class MainActivity extends FragmentActivity {
 				fragment.setArguments(args);
 				break;
 
-			case INDEX_DICO_EXPRESSION:
+			case R.id.navigation_expression:
 				mFragmentTitle = R.string.menu_subitem_expression;
 				fragment = new DicoFragment();
 				args = new Bundle();
@@ -212,27 +181,28 @@ public class MainActivity extends FragmentActivity {
 				fragment.setArguments(args);
 				break;
 
-			case INDEX_REVIEW:
+			case R.id.navigation_review:
 				mFragmentTitle = R.string.param_review;
 				fragment = new ReviewParametersFragment();
 				break;
 
-			case INDEX_TEST:
+			case R.id.navigation_test:
 				mFragmentTitle = R.string.param_test;
 				fragment = new TestParametersFragment();
 				break;
 
-			case INDEX_KANA:
+			case R.id.navigation_kana:
 				mFragmentTitle = R.string.menu_subitem_kana;
 				fragment = new HelpKanaFragment();
+//				fragment = new BlankFragment();
 				break;
 
-			case INDEX_PARAMETERS:
+			case R.id.navigation_parameters:
 				mFragmentTitle = R.string.menu_subitem_parameters;
 				fragment = new ParametersFragment();
 				break;
 
-			case INDEX_LESSONS:
+			case R.id.navigation_lessons:
 				mFragmentTitle = R.string.menu_subitem_lessons;
 				fragment = new LessonsFragment();
 				break;
@@ -244,30 +214,30 @@ public class MainActivity extends FragmentActivity {
 		// Insert the fragment by replacing any existing fragment
 		final FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
 		fragmentTransaction.replace(R.id.content_frame, fragment);
-		fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+//		fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
 		fragmentTransaction.commit();
 
-		mDrawerLayout.closeDrawer(mDrawerRelativeLayout);
+		mDrawerLayout.closeDrawers();
 		if (invalideOptionsMenu) {
 			invalidateOptionsMenu();
 		}
 
-		CURRENT_INDEX = index;
+		CURRENT_INDEX = id;
 		setTitle(mFragmentTitle);
 	}
 
 	@Override
 	public void setTitle(int title) {
-//        mFragmentTitle = title;
-		if (getActionBar() != null)
-			getActionBar().setTitle(title);
+		if (toolbar != null) {
+			toolbar.setTitle(title);
+		}
 	}
 
 	@Override
 	public void setTitle(CharSequence title) {
-//        mFragmentTitle = title;
-		if (getActionBar() != null)
-			getActionBar().setTitle(title);
+		if (toolbar != null) {
+			toolbar.setTitle(title);
+		}
 	}
 
 	/**
@@ -315,7 +285,7 @@ public class MainActivity extends FragmentActivity {
 	private void showResults(String query) {
 		Bundle args = new Bundle();
 		args.putString("query", query);
-		args.putSerializable("type", CURRENT_INDEX == INDEX_DICO_WORD ? Type.WORD : Type.EXPRESSION);
+		args.putSerializable("type", CURRENT_INDEX == R.id.navigation_word ? Type.WORD : Type.EXPRESSION);
 
 		final DicoFragment fragment = new DicoFragment();
 		fragment.setArguments(args);
@@ -324,26 +294,20 @@ public class MainActivity extends FragmentActivity {
 
 		setTitle(R.string.menu_subitem_word);
 
-		mDrawerLayout.closeDrawer(mDrawerRelativeLayout);
+		mDrawerLayout.closeDrawers();
 	}
 
 	@Override
 	public void onBackPressed() {
-		if (CURRENT_INDEX != INDEX_DICO_WORD) {
-			selectItemAtIndex(INDEX_DICO_WORD, true);
+		if (CURRENT_INDEX != R.id.navigation_word) {
+			selectItemAtIndex(R.id.navigation_word, true);
 		} else {
 			// todo : un toast demandant un back une seconde fois pour sortir
 			new AlertDialog.Builder(this)
 					.setIcon(android.R.drawable.ic_dialog_alert)
 					.setTitle(R.string.closing_activity_title)
 					.setMessage(R.string.closing_activity_message)
-					.setPositiveButton(getString(R.string.yes), new DialogInterface.OnClickListener() {
-						@Override
-						public void onClick(DialogInterface dialog, int which) {
-							finish();
-						}
-
-					})
+					.setPositiveButton(getString(R.string.yes), (dialog, which) -> finish())
 					.setNegativeButton(getString(R.string.no), null)
 					.show();
 		}
