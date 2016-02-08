e7d10376dec105f7d6a4d39140330c2448bcce1d
Le Gall Benoit <legall.benoit@gmail.com>
2016-02-08 21:10:28 +0100

    use lambda

--- app/src/main/java/fr/frogdevelopment/nihongo/SplashScreenActivity.java
+++ app/src/main/java/fr/frogdevelopment/nihongo/SplashScreenActivity.java
@@ -20,18 +20,14 @@ public class SplashScreenActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        new Handler().postDelayed(new Runnable() {
+        new Handler().postDelayed(() -> {
+            // This method will be executed once the timer is over
+            // Start your app main activity
+            Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+            startActivity(i);
 
-            @Override
-            public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
-                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
-                startActivity(i);
-
-                // close this activity
-                finish();
-            }
+            // close this activity
+            finish();
         }, SPLASH_TIME_OUT);
     }
 
