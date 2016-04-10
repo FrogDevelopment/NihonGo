9bfef862f793e3f886af6b8ab6ad2b8f49a6483e
Le Gall Benoit <legall.benoit@gmail.com>
2016-04-10 16:20:12 +0200

    shorter splash screen

--- app/src/main/java/fr/frogdevelopment/nihongo/SplashScreenActivity.java
+++ app/src/main/java/fr/frogdevelopment/nihongo/SplashScreenActivity.java
@@ -13,7 +13,7 @@ import android.os.Handler;
 public class SplashScreenActivity extends Activity {
 
     // Splash screen timer
-    private static final long SPLASH_TIME_OUT = 2000;
+    private static final long SPLASH_TIME_OUT = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
