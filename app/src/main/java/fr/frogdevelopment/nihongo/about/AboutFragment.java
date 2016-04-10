fa5f4a8b261a35dfdd33687add36b913ef91cb7c
Le Gall Benoit <legall.benoit@gmail.com>
2016-04-10 21:08:02 +0200

    new logos

--- app/src/main/java/fr/frogdevelopment/nihongo/about/AboutFragment.java
+++ app/src/main/java/fr/frogdevelopment/nihongo/about/AboutFragment.java
@@ -21,8 +21,6 @@ public class AboutFragment extends Fragment  {
 
 	@Bind(R.id.about_logos_link)
 	TextView logos;
-	@Bind(R.id.about_drawer_header_link)
-	TextView drawerHeader;
 	@Bind(R.id.about_hiragana_link)
 	TextView hiragana;
 	@Bind(R.id.about_katakana_link)
@@ -37,7 +35,6 @@ public class AboutFragment extends Fragment  {
 
 		MovementMethod instance = LinkMovementMethod.getInstance();
 		logos.setMovementMethod(instance);
-		drawerHeader.setMovementMethod(instance);
 		hiragana.setMovementMethod(instance);
 		katakana.setMovementMethod(instance);
 
