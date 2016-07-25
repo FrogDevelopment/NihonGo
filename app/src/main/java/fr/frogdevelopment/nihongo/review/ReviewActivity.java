9fe1e535d655864a760708d0d469582aba1dbc1b
Benoît Le Gall <benoit.legall@bio-c-bon.fr>
2016-07-25 13:55:34 +0200

    add sort selection on review

--- app/src/main/java/fr/frogdevelopment/nihongo/review/ReviewActivity.java
+++ app/src/main/java/fr/frogdevelopment/nihongo/review/ReviewActivity.java
@@ -34,174 +34,185 @@ import fr.frogdevelopment.nihongo.preferences.PreferencesHelper;
 
 public class ReviewActivity extends AppCompatActivity implements LoaderCallbacks<Cursor>, ReviewFragment.OnFragmentInteractionListener {
 
-	private static final int LOADER_ID = 710;
-	private ReviewAdapter adapter;
+    private static final int LOADER_ID = 710;
+    private ReviewAdapter adapter;
 
-	@BindView(R.id.toolbar)
-	Toolbar toolbar;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
-	@BindView(R.id.review_viewpager)
-	ViewPager viewPager;
+    @BindView(R.id.review_viewpager)
+    ViewPager viewPager;
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-			case android.R.id.home:
-				back();
-				return true;
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                back();
+                return true;
 
-			default:
-				return super.onOptionsItemSelected(item);
-		}
-	}
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
-	@Override
-	public void onBackPressed() {
-		back();
-	}
+    @Override
+    public void onBackPressed() {
+        back();
+    }
 
-	private void back() {
-		NavUtils.navigateUpFromSameTask(this);
-		overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-	}
+    private void back() {
+        NavUtils.navigateUpFromSameTask(this);
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+    }
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		setContentView(R.layout.activity_review);
+        setContentView(R.layout.activity_review);
 
-		ButterKnife.bind(this);
+        ButterKnife.bind(this);
 
-		initToolbar();
+        initToolbar();
 
-		final boolean isJapaneseReviewed = getIntent().getExtras().getBoolean("isJapaneseReviewed");
+        final boolean isJapaneseReviewed = getIntent().getExtras().getBoolean("isJapaneseReviewed");
 
-		adapter = new ReviewAdapter(getFragmentManager(), isJapaneseReviewed);
-		viewPager.setAdapter(adapter);
+        adapter = new ReviewAdapter(getFragmentManager(), isJapaneseReviewed);
+        viewPager.setAdapter(adapter);
 
-		viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-			@Override
-			public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
-			}
+            }
 
-			@Override
-			public void onPageSelected(int position) {
-				ReviewFragment fragment = adapter.getItemAt(position);
-				if (position + 1 == adapter.getCount()) {
-					if (fragment.mFabAgain.getVisibility() == View.GONE) {
-						fragment.mFabAgain.show();
-					}
-				} else {
-					if (fragment.mFabAgain.getVisibility() == View.VISIBLE) {
-						fragment.mFabAgain.hide();
-					}
-				}
-			}
+            @Override
+            public void onPageSelected(int position) {
+                ReviewFragment fragment = adapter.getItemAt(position);
+                if (position + 1 == adapter.getCount()) {
+                    if (fragment.mFabAgain.getVisibility() == View.GONE) {
+                        fragment.mFabAgain.show();
+                    }
+                } else {
+                    if (fragment.mFabAgain.getVisibility() == View.VISIBLE) {
+                        fragment.mFabAgain.hide();
+                    }
+                }
+            }
 
-			@Override
-			public void onPageScrollStateChanged(int state) {
+            @Override
+            public void onPageScrollStateChanged(int state) {
 
-			}
-		});
+            }
+        });
 
-		getLoaderManager().initLoader(LOADER_ID, getIntent().getExtras(), this);
+        getLoaderManager().initLoader(LOADER_ID, getIntent().getExtras(), this);
 
-		boolean doNotshow = PreferencesHelper.getInstance(getApplicationContext()).getBoolean(Preferences.HELP_REVIEW);
-		if (!doNotshow) {
-			HelpDialog.show(getFragmentManager(), R.layout.dialog_help_review, true);
-		}
-	}
+        boolean doNotshow = PreferencesHelper.getInstance(getApplicationContext()).getBoolean(Preferences.HELP_REVIEW);
+        if (!doNotshow) {
+            HelpDialog.show(getFragmentManager(), R.layout.dialog_help_review, true);
+        }
+    }
 
 
-	private void initToolbar() {
-		setSupportActionBar(toolbar);
-		final ActionBar actionBar = getSupportActionBar();
+    private void initToolbar() {
+        setSupportActionBar(toolbar);
+        final ActionBar actionBar = getSupportActionBar();
 
-		if (actionBar != null) {
-			actionBar.setDisplayHomeAsUpEnabled(true);
-			actionBar.setHomeButtonEnabled(true);
-		}
-	}
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeButtonEnabled(true);
+        }
+    }
 
-	@Override
-	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
 
-		final String count = args.getString("count");
-		String limit = "";
-		if (NumberUtils.isNumber(count)) {
-			limit = " LIMIT " + Integer.parseInt(count);
-		}
-		final String[] tags = args.getStringArray("tags");
+        final String count = args.getString("count");
+        String limit = "";
+        if (NumberUtils.isNumber(count)) {
+            limit = " LIMIT " + Integer.parseInt(count);
+        }
+        final String[] tags = args.getStringArray("tags");
 
-		String selection = "1 = 1";
-		String[] likes = null;
-		if (ArrayUtils.isNotEmpty(tags)) {
-			for (String tag : tags) {
-				likes = ArrayUtils.add(likes, DicoContract.TAGS + " LIKE '%" + tag + "%'");
-			}
-			selection += " AND (" + StringUtils.join(likes, " OR ") + ")";
-		}
+        String selection = "1 = 1";
+        String[] likes = null;
+        if (ArrayUtils.isNotEmpty(tags)) {
+            for (String tag : tags) {
+                likes = ArrayUtils.add(likes, DicoContract.TAGS + " LIKE '%" + tag + "%'");
+            }
+            selection += " AND (" + StringUtils.join(likes, " OR ") + ")";
+        }
 
-		final boolean excludeLearned = args.getBoolean("excludeLearned");
-		if (excludeLearned) {
-			selection += " AND LEARNED = '0'";
-		}
+        final boolean excludeLearned = args.getBoolean("excludeLearned");
+        if (excludeLearned) {
+            selection += " AND LEARNED = '0'";
+        }
 
-		final boolean onlyFavorite = args.getBoolean("onlyFavorite");
-		if (onlyFavorite) {
-			selection += " AND FAVORITE = '1'";
-		}
+        final boolean onlyFavorite = args.getBoolean("onlyFavorite");
+        if (onlyFavorite) {
+            selection += " AND FAVORITE = '1'";
+        }
 
-		String sortOrder;
-		final boolean isRandom = args.getBoolean("isRandom");
-		if (isRandom) {
-			sortOrder = "RANDOM()" + limit;
-		} else {
-			sortOrder = "_id DESC" + limit;
-		}
-		return new CursorLoader(this, NihonGoContentProvider.URI_WORD, DicoContract.COLUMNS, selection, null, sortOrder);
-	}
+        String sortOrder;
+        final int sort = args.getInt("sort");
 
-	@Override
-	public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-		adapter.setData(data);
-		data.close();
-	}
+        switch (sort) {
+            case 0: // new -> old
+                sortOrder = "_id ASC" + limit;
+                break;
+            case 1: // old -> new
+                sortOrder = "_id DESC" + limit;
+                break;
+            case 2: // random
+                sortOrder = "RANDOM()" + limit;
+                break;
+            default:
+                sortOrder = "";
+                break;
+        }
 
-	@Override
-	public void onLoaderReset(Loader<Cursor> loader) {
-		// TODO Auto-generated method stub
-	}
+        return new CursorLoader(this, NihonGoContentProvider.URI_WORD, DicoContract.COLUMNS, selection, null, sortOrder);
+    }
 
-	@Override
-	public void setFavorite(Item item) {
-		final ContentValues values = new ContentValues();
-		values.put(DicoContract.FAVORITE, item.favorite);
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        adapter.setData(data);
+        data.close();
+    }
 
-		updateItem(item, values);
-	}
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        // TODO Auto-generated method stub
+    }
 
-	@Override
-	public void setLearned(Item item) {
-		final ContentValues values = new ContentValues();
-		values.put(DicoContract.LEARNED, item.learned);
+    @Override
+    public void setFavorite(Item item) {
+        final ContentValues values = new ContentValues();
+        values.put(DicoContract.FAVORITE, item.favorite);
 
-		updateItem(item, values);
-	}
+        updateItem(item, values);
+    }
 
-	private void updateItem(Item item, ContentValues values) {
-		final String where = DicoContract._ID + "=?";
-		final String[] selectionArgs = {item.id};
+    @Override
+    public void setLearned(Item item) {
+        final ContentValues values = new ContentValues();
+        values.put(DicoContract.LEARNED, item.learned);
 
-		getContentResolver().update(NihonGoContentProvider.URI_WORD, values, where, selectionArgs);
-	}
+        updateItem(item, values);
+    }
 
-	@Override
-	public void reviewAgain() {
-		viewPager.setAdapter(adapter);
-		getLoaderManager().restartLoader(LOADER_ID, getIntent().getExtras(), this);
-	}
+    private void updateItem(Item item, ContentValues values) {
+        final String where = DicoContract._ID + "=?";
+        final String[] selectionArgs = {item.id};
+
+        getContentResolver().update(NihonGoContentProvider.URI_WORD, values, where, selectionArgs);
+    }
+
+    @Override
+    public void reviewAgain() {
+        viewPager.setAdapter(adapter);
+        getLoaderManager().restartLoader(LOADER_ID, getIntent().getExtras(), this);
+    }
 
 }
--- app/src/main/java/fr/frogdevelopment/nihongo/review/ReviewParametersFragment.java
+++ app/src/main/java/fr/frogdevelopment/nihongo/review/ReviewParametersFragment.java
@@ -41,147 +41,163 @@ import fr.frogdevelopment.nihongo.dialog.TagsDialog;
 
 public class ReviewParametersFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor>, TagsDialog.TagDialogListener {
 
-	private static final int LOADER_ID = 700;
+    private static final int LOADER_ID = 700;
 
-	@BindView(R.id.review_switch_language)
-	Switch   mSwitchLanguageView;
-	@BindView(R.id.review_switch_sort)
-	Switch   mSwitchSortView;
-	@BindView(R.id.review_switch_learned)
-	Switch   mSwitchLearned;
-	@BindView(R.id.review_switch_favorite)
-	Switch   mSwitchFavorite;
-	@BindView(R.id.review_param_quantity_selection)
-	TextView mQuantitySelected;
-	@BindView(R.id.review_param_tag_selection)
-	TextView mTagSelected;
-	@BindView(R.id.review_button_start)
-	Button   startButton;
+    @BindView(R.id.review_switch_language)
+    Switch mSwitchLanguageView;
+    @BindView(R.id.review_param_sort_selection)
+    TextView mSortSelected;
+    @BindView(R.id.review_switch_learned)
+    Switch mSwitchLearned;
+    @BindView(R.id.review_switch_favorite)
+    Switch mSwitchFavorite;
+    @BindView(R.id.review_param_quantity_selection)
+    TextView mQuantitySelected;
+    @BindView(R.id.review_param_tag_selection)
+    TextView mTagSelected;
+    @BindView(R.id.review_button_start)
+    Button startButton;
 
-	private String[] quantities;
-	private String selectedQuantity = null;
-	private ArrayList<Integer> mSelectedItems;
-	private String[]           mSelectedTags;
-	private List<String>       items;
-	private Unbinder unbinder;
+    private int selectedSort = -1;
+    private String[] quantities;
+    private String selectedQuantity = null;
+    private ArrayList<Integer> mSelectedItems;
+    private String[] mSelectedTags;
+    private List<String> items;
+    private Unbinder unbinder;
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.fragment_review_parameters, container, false);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_review_parameters, container, false);
 
-		unbinder = ButterKnife.bind(this, rootView);
+        unbinder = ButterKnife.bind(this, rootView);
 
-		getLoaderManager().initLoader(LOADER_ID, null, this);
+        getLoaderManager().initLoader(LOADER_ID, null, this);
 
-		quantities = getResources().getStringArray(R.array.param_quantities);
-		quantities = ArrayUtils.add(quantities, getResources().getString(R.string.param_quantity_all));
+        quantities = getResources().getStringArray(R.array.param_quantities);
+        quantities = ArrayUtils.add(quantities, getResources().getString(R.string.param_quantity_all));
 
-		if (savedInstanceState != null) {
-			mSwitchLanguageView.setChecked(savedInstanceState.getBoolean("isJapaneseReviewed"));
-			mSwitchSortView.setChecked(savedInstanceState.getBoolean("isRandom"));
-			selectedQuantity = savedInstanceState.getString("count");
-			mQuantitySelected.setText(selectedQuantity);
-			mSelectedTags = savedInstanceState.getStringArray("tags");
-			mTagSelected.setText(StringUtils.join(mSelectedTags, ", "));
-			mSwitchLearned.setChecked(savedInstanceState.getBoolean("excludeLearned"));
-			mSwitchFavorite.setChecked(savedInstanceState.getBoolean("onlyFavorite"));
-		}
+        if (savedInstanceState != null) {
+            mSwitchLanguageView.setChecked(savedInstanceState.getBoolean("isJapaneseReviewed"));
+            selectedSort = savedInstanceState.getInt("sort");
+            mSortSelected.setText(getResources().getStringArray(R.array.param_sorts)[selectedSort]);
+            selectedQuantity = savedInstanceState.getString("count");
+            mQuantitySelected.setText(selectedQuantity);
+            mSelectedTags = savedInstanceState.getStringArray("tags");
+            mTagSelected.setText(StringUtils.join(mSelectedTags, ", "));
+            mSwitchLearned.setChecked(savedInstanceState.getBoolean("excludeLearned"));
+            mSwitchFavorite.setChecked(savedInstanceState.getBoolean("onlyFavorite"));
+        }
 
-		return rootView;
-	}
+        return rootView;
+    }
 
-	@Override
-	public void onDestroyView() {
-		super.onDestroyView();
-		unbinder.unbind();
-	}
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
 
-	@Override
-	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-		Uri uri = Uri.parse(NihonGoContentProvider.URI_WORD + "/TAGS");
-		return new CursorLoader(getActivity(), uri, new String[]{DicoContract.TAGS}, null, null, null);
-	}
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        Uri uri = Uri.parse(NihonGoContentProvider.URI_WORD + "/TAGS");
+        return new CursorLoader(getActivity(), uri, new String[]{DicoContract.TAGS}, null, null, null);
+    }
 
-	@Override
-	public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-		Set<String> uniqueItems = new HashSet<>();
-		while (data.moveToNext()) {
-			String row = data.getString(0);
-			String[] tags = row.split(",");
-			uniqueItems.addAll(Arrays.asList(tags));
-		}
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        Set<String> uniqueItems = new HashSet<>();
+        while (data.moveToNext()) {
+            String row = data.getString(0);
+            String[] tags = row.split(",");
+            uniqueItems.addAll(Arrays.asList(tags));
+        }
 
-		items = new ArrayList<>(uniqueItems);
+        items = new ArrayList<>(uniqueItems);
 
-		Collections.sort(items);
+        Collections.sort(items);
 
-		data.close();
-	}
+        data.close();
+    }
 
-	@Override
-	public void onLoaderReset(Loader<Cursor> loader) {
-	}
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+    }
 
-	@OnClick(R.id.review_param_quantity)
-	void onClickQuantity(View v) {
-		new AlertDialog.Builder(getActivity())
-				.setTitle(R.string.param_quantity_selection)
-				.setItems(quantities, (dialog, which) -> {
-					selectedQuantity = quantities[which];
-					mQuantitySelected.setText(selectedQuantity);
+    @OnClick(R.id.review_param_sort)
+    public void onClickType(View v) {
+        new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.param_type_selection)
+                .setItems(R.array.param_sorts, (dialog, which) -> {
+                    selectedSort = which;
+                    mSortSelected.setText(getResources().getStringArray(R.array.param_sorts)[which]);
 
-					startButton.setEnabled(selectedQuantity != null);
-				})
-				.create()
-				.show();
-	}
+                    startButton.setEnabled(selectedSort > -1 && selectedQuantity != null);
+                })
+                .create()
+                .show();
+    }
 
-	@OnClick(R.id.review_param_tag)
-	void onClickTag() {
-		TagsDialog.show(getFragmentManager(), this, items, mSelectedItems);
-	}
+    @OnClick(R.id.review_param_quantity)
+    void onClickQuantity(View v) {
+        new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.param_sort_selection)
+                .setItems(quantities, (dialog, which) -> {
+                    selectedQuantity = quantities[which];
+                    mQuantitySelected.setText(selectedQuantity);
 
-	@Override
-	public void onReturnValue(ArrayList<Integer> selectedItems) {
-		mSelectedItems = selectedItems;
-		mSelectedTags = null;
+                    startButton.setEnabled(selectedSort > -1 && selectedQuantity != null);
+                })
+                .create()
+                .show();
+    }
 
-		for (Integer selectedIndex : mSelectedItems) {
-			String selectedTag = items.get(selectedIndex);
-			mSelectedTags = ArrayUtils.add(mSelectedTags, selectedTag);
-		}
+    @OnClick(R.id.review_param_tag)
+    void onClickTag() {
+        TagsDialog.show(getFragmentManager(), this, items, mSelectedItems);
+    }
 
-		mTagSelected.setText(StringUtils.join(mSelectedTags, ", "));
-	}
+    @Override
+    public void onReturnValue(ArrayList<Integer> selectedItems) {
+        mSelectedItems = selectedItems;
+        mSelectedTags = null;
 
-	@OnClick(R.id.review_button_start)
-	void onClickButtonStart() {
-		Bundle options = new Bundle();
-		populateUiSelection(options);
+        for (Integer selectedIndex : mSelectedItems) {
+            String selectedTag = items.get(selectedIndex);
+            mSelectedTags = ArrayUtils.add(mSelectedTags, selectedTag);
+        }
 
-		Intent intent = new Intent(getActivity(), ReviewActivity.class);
-		intent.putExtras(options);
+        mTagSelected.setText(StringUtils.join(mSelectedTags, ", "));
+    }
 
-		startActivity(intent);
-		getActivity().overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-	}
+    @OnClick(R.id.review_button_start)
+    void onClickButtonStart() {
+        Bundle options = new Bundle();
+        populateUiSelection(options);
 
-	private void populateUiSelection(Bundle options) {
-		options.putBoolean("isJapaneseReviewed", mSwitchLanguageView.isChecked());
-		options.putBoolean("isRandom", mSwitchSortView.isChecked());
-		options.putString("count", selectedQuantity);
-		options.putStringArray("tags", mSelectedTags);
-		options.putBoolean("excludeLearned", mSwitchLearned.isChecked());
-		options.putBoolean("onlyFavorite", mSwitchFavorite.isChecked());
-	}
+        Intent intent = new Intent(getActivity(), ReviewActivity.class);
+        intent.putExtras(options);
 
-	@Override
-	public void onSaveInstanceState(Bundle savedInstanceState) {
+        startActivity(intent);
+        getActivity().overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+    }
 
-		// Store UI state to the savedInstanceState.
-		populateUiSelection(savedInstanceState);
+    private void populateUiSelection(Bundle options) {
+        options.putBoolean("isJapaneseReviewed", mSwitchLanguageView.isChecked());
+        options.putInt("sort", selectedSort);
+        options.putString("count", selectedQuantity);
+        options.putStringArray("tags", mSelectedTags);
+        options.putBoolean("excludeLearned", mSwitchLearned.isChecked());
+        options.putBoolean("onlyFavorite", mSwitchFavorite.isChecked());
+    }
 
-		super.onSaveInstanceState(savedInstanceState);
-	}
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+
+        // Store UI state to the savedInstanceState.
+        populateUiSelection(savedInstanceState);
+
+        super.onSaveInstanceState(savedInstanceState);
+    }
 
 }
