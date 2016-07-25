32c83f0a08e3cac989c6349a5e979399b88d9d48
Benoît Le Gall <benoit.legall@bio-c-bon.fr>
2016-07-25 14:04:20 +0200

    handle ArrayIndexOutOfBoundsException

--- app/src/main/java/fr/frogdevelopment/nihongo/test/TestSelectActivity.java
+++ app/src/main/java/fr/frogdevelopment/nihongo/test/TestSelectActivity.java
@@ -26,212 +26,216 @@ import fr.frogdevelopment.nihongo.data.Item;
 
 public class TestSelectActivity extends TestAbstractActivity {
 
-	private static final int LOADER_ID_ITEMS_QCM = 720;
+    private static final int LOADER_ID_ITEMS_QCM = 720;
 
-	@BindView(R.id.test_select_to_find)
-	TextView mToFindView;
+    @BindView(R.id.test_select_to_find)
+    TextView mToFindView;
 
-	@BindView(R.id.test_select_answers)
-	LinearLayout answers;
+    @BindView(R.id.test_select_answers)
+    LinearLayout answers;
 
-	private List<Item> itemsQCM = new ArrayList<>();
+    private List<Item> itemsQCM = new ArrayList<>();
 
-	public TestSelectActivity() {
-		super(R.layout.activity_test_select);
-	}
+    public TestSelectActivity() {
+        super(R.layout.activity_test_select);
+    }
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		for (int i = 0; i < nbAnswer; i++) {
-			Button button = new Button(this);
-			answers.addView(button);
-			button.setOnClickListener(view -> {
-				Button answerButton = (Button) view;
-				CharSequence testAnswer = answerButton.getText();
+        for (int i = 0; i < nbAnswer; i++) {
+            Button button = new Button(this);
+            answers.addView(button);
+            button.setOnClickListener(view -> {
+                Button answerButton = (Button) view;
+                CharSequence testAnswer = answerButton.getText();
 
-				validate(testAnswer);
-			});
-		}
-	}
+                validate(testAnswer);
+            });
+        }
+    }
 
-	@Override
-	public Loader<Cursor> onCreateLoader(int id, Bundle options) {
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle options) {
 
-		// To find items
-		if (id == TestAbstractActivity.LOADER_ID_ITEMS_TO_FIND) {
-			return super.onCreateLoader(id, options);
-		} else { // QCM items
-			String selection = "INPUT != '~'"; // fixme
+        // To find items
+        if (id == TestAbstractActivity.LOADER_ID_ITEMS_TO_FIND) {
+            return super.onCreateLoader(id, options);
+        } else { // QCM items
+            String selection = "INPUT != '~'"; // fixme
 
-			switch (typeTest) {
+            switch (typeTest) {
 
-				case 0: // Kanji -> Hiragana
-				case 1: // Hiragana -> Kanji
-					// katakana exclude
-					selection += " AND KANJI IS NOT NULL AND KANJI != ''";
-					break;
+                case 0: // Kanji -> Hiragana
+                case 1: // Hiragana -> Kanji
+                    // katakana exclude
+                    selection += " AND KANJI IS NOT NULL AND KANJI != ''";
+                    break;
 
-				case 2: // Japanese -> French
-				case 3: // French -> Japanese
-					break;
-			}
+                case 2: // Japanese -> French
+                case 3: // French -> Japanese
+                    break;
+            }
 
-			String[] selectionArgs = null;
-			List<String> idsToFind = options.getStringArrayList("idsToFind");
-			if (idsToFind != null) {
-				StringBuilder inList = new StringBuilder(idsToFind.size());
-				selectionArgs = new String[idsToFind.size()];
-				int i = 0;
-				for (String idDone : idsToFind) {
-					if (i > 0) {
-						inList.append(",");
-					}
-					inList.append("?");
+            String[] selectionArgs = null;
+            List<String> idsToFind = options.getStringArrayList("idsToFind");
+            if (idsToFind != null) {
+                StringBuilder inList = new StringBuilder(idsToFind.size());
+                selectionArgs = new String[idsToFind.size()];
+                int i = 0;
+                for (String idDone : idsToFind) {
+                    if (i > 0) {
+                        inList.append(",");
+                    }
+                    inList.append("?");
 
-					selectionArgs[i] = idDone;
-					i++;
-				}
+                    selectionArgs[i] = idDone;
+                    i++;
+                }
 
-				selection += "AND _ID NOT IN (" + inList.toString() + ")";
-			}
+                selection += "AND _ID NOT IN (" + inList.toString() + ")";
+            }
 
-			String sortOrder = "RANDOM() LIMIT " + quantityMax * nbAnswer;
+            String sortOrder = "RANDOM() LIMIT " + quantityMax * nbAnswer;
 
-			return new CursorLoader(this, NihonGoContentProvider.URI_WORD, DicoContract.COLUMNS, selection, selectionArgs, sortOrder);
-		}
-	}
+            return new CursorLoader(this, NihonGoContentProvider.URI_WORD, DicoContract.COLUMNS, selection, selectionArgs, sortOrder);
+        }
+    }
 
-	@Override
-	public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
 
-		// To find items
-		if (loader.getId() == TestAbstractActivity.LOADER_ID_ITEMS_TO_FIND) {
-			quantityMax = data.getCount();
-			results = new ArrayList<>(quantityMax);
+        // To find items
+        if (loader.getId() == TestAbstractActivity.LOADER_ID_ITEMS_TO_FIND) {
+            quantityMax = data.getCount();
+            results = new ArrayList<>(quantityMax);
 
-			displayQuantity();
+            displayQuantity();
 
-			ArrayList<String> idsToFind = new ArrayList<>();
-			Item item;
-			while (data.moveToNext()) {
-				item = new Item(data);
-				itemsToFind.add(item);
-				idsToFind.add(item.id);
-			}
+            ArrayList<String> idsToFind = new ArrayList<>();
+            Item item;
+            while (data.moveToNext()) {
+                item = new Item(data);
+                itemsToFind.add(item);
+                idsToFind.add(item.id);
+            }
 
-			Bundle bundle = new Bundle();
-			bundle.putStringArrayList("idsToFind", idsToFind);
+            Bundle bundle = new Bundle();
+            bundle.putStringArrayList("idsToFind", idsToFind);
 
-			data.close();
+            data.close();
 
-			getLoaderManager().restartLoader(LOADER_ID_ITEMS_QCM, bundle, this);
+            getLoaderManager().restartLoader(LOADER_ID_ITEMS_QCM, bundle, this);
 
-		} else {// QCM items
-			while (data.moveToNext()) {
-				itemsQCM.add(new Item(data));
-			}
+        } else {// QCM items
+            while (data.moveToNext()) {
+                itemsQCM.add(new Item(data));
+            }
 
-			data.close();
-			next(itemsToFind.get(currentItemIndex));
-		}
-	}
+            data.close();
+            next(itemsToFind.get(currentItemIndex));
+        }
+    }
 
-	@Override
-	protected void next(Item item) {
-		int testIndex = new Random().nextInt(nbAnswer);
+    @Override
+    protected void next(Item item) {
+        int testIndex = new Random().nextInt(nbAnswer);
 
-		Button mResponseView;
-		String toFind;
-		String answer;
+        Button mResponseView;
+        String toFind;
+        String answer;
 
-		for (int index = 0; index < nbAnswer; index++) {
-			mResponseView = (Button) answers.getChildAt(index);
+        for (int index = 0; index < nbAnswer; index++) {
+            mResponseView = (Button) answers.getChildAt(index);
 
-			if (testIndex != index) { // QCM
-				answer = getButtonLabel(itemsQCM.remove(0));
-			} else {// RESPONSE
-				answer = getButtonLabel(item);
-				toFind = getTestLabel(item);
+            if (testIndex != index) { // QCM
+                answer = getButtonLabel(itemsQCM.remove(0));
+            } else {// RESPONSE
+                answer = getButtonLabel(item);
+                toFind = getTestLabel(item);
 
-				mToFindView.setText(toFind);
+                mToFindView.setText(toFind);
 
-				results.add(new Result(toFind, answer, false));
-			}
+                results.add(new Result(toFind, answer, false));
+            }
 
-			mResponseView.setText(answer);
-		}
-	}
+            mResponseView.setText(answer);
+        }
+    }
 
-	private String getButtonLabel(Item item) {
-		String answer = null;
+    private String getButtonLabel(Item item) {
+        String answer = null;
 
-		switch (typeTest) {
+        switch (typeTest) {
 
-			case 0: // Kanji -> Hiragana
-				answer = item.kana;
-				break;
+            case 0: // Kanji -> Hiragana
+                answer = item.kana;
+                break;
 
-			case 1: // Hiragana -> Kanji
-				if (item.kanji.contains("、")) {
-					String[] kanjis = item.kanji.split("、");
-					int i = 0;
-					while (i < 1) {
-						i = new Random().nextInt(2);
-					}
-					answer = kanjis[i - 1];
-				} else
-					answer = item.kanji;
-				break;
+            case 1: // Hiragana -> Kanji
+                if (item.kanji.contains("、")) {
+                    String[] kanjis = item.kanji.split("、");
+                    int i = 0;
+                    while (i < 1) {
+                        i = new Random().nextInt(2);
+                    }
+                    answer = kanjis[i - 1];
+                } else
+                    answer = item.kanji;
+                break;
 
-			case 2: // Japanese -> French
-				answer = item.input;
-				break;
+            case 2: // Japanese -> French
+                answer = item.input;
+                break;
 
-			case 3: // French -> Japanese
-				if (isDisplayKanji && StringUtils.isNotBlank(item.kanji)) {
-					answer = item.kanji;
-				} else {
-					answer = item.kana;
-				}
-				break;
-		}
+            case 3: // French -> Japanese
+                if (isDisplayKanji && StringUtils.isNotBlank(item.kanji)) {
+                    answer = item.kanji;
+                } else {
+                    answer = item.kana;
+                }
+                break;
+        }
 
-		return answer;
-	}
+        return answer;
+    }
 
-	private String getTestLabel(Item item) {
-		String toFind = null;
+    private String getTestLabel(Item item) {
+        String toFind = null;
 
-		switch (typeTest) {
-			case 0: // Kanji -> Hiragana
-				if (item.kanji.contains("、")) {
-					String[] kanjis = item.kanji.split("、");
-					toFind = kanjis[new Random().nextInt(2) - 1];
-				} else
-					toFind = item.kanji;
-				break;
+        switch (typeTest) {
+            case 0: // Kanji -> Hiragana
+                if (item.kanji.contains("、")) {
+                    String[] kanjis = item.kanji.split("、");
+                    int i = 0;
+                    while (i < 1) {
+                        i = new Random().nextInt(2);
+                    }
+                    toFind = kanjis[i - 1];
+                } else
+                    toFind = item.kanji;
+                break;
 
-			case 1: // Hiragana -> Kanji
-				toFind = item.kana;
-				break;
+            case 1: // Hiragana -> Kanji
+                toFind = item.kana;
+                break;
 
-			case 2: // Japanese -> French
+            case 2: // Japanese -> French
 
-				if (isDisplayKanji && StringUtils.isNoneBlank(item.kanji)) {
-					toFind = item.kanji;
-				} else {
-					toFind = item.kana;
-				}
-				break;
+                if (isDisplayKanji && StringUtils.isNoneBlank(item.kanji)) {
+                    toFind = item.kanji;
+                } else {
+                    toFind = item.kana;
+                }
+                break;
 
-			case 3: // French -> Japanese
-				toFind = item.input;
-				break;
-		}
+            case 3: // French -> Japanese
+                toFind = item.input;
+                break;
+        }
 
-		return toFind;
-	}
+        return toFind;
+    }
 
 }
