2ae08b3420221e66b5c795bc6f32e3a316d9d55e
Benoît Le Gall <benoit.legall@bio-c-bon.fr>
2016-10-25 17:36:52 +0200

    add confirmation when empty result

--- app/src/main/java/fr/frogdevelopment/nihongo/test/TestInputActivity.java
+++ app/src/main/java/fr/frogdevelopment/nihongo/test/TestInputActivity.java
@@ -4,6 +4,9 @@
 
 package fr.frogdevelopment.nihongo.test;
 
+import android.app.AlertDialog;
+import android.text.Editable;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ScrollView;
@@ -20,72 +23,82 @@ import fr.frogdevelopment.nihongo.data.Item;
 
 public class TestInputActivity extends TestAbstractActivity {
 
-	@BindView(R.id.test_input_scroll)
-	ScrollView mScrollView;
+    @BindView(R.id.test_input_scroll)
+    ScrollView mScrollView;
 
-	@BindView(R.id.test_input_to_find)
-	TextView toFindView;
+    @BindView(R.id.test_input_to_find)
+    TextView toFindView;
 
-	@BindView(R.id.test_input_answer)
-	EditText answerView;
+    @BindView(R.id.test_input_answer)
+    EditText answerView;
 
-	public TestInputActivity() {
-		super(R.layout.activity_test_input);
-	}
+    public TestInputActivity() {
+        super(R.layout.activity_test_input);
+    }
 
-	@OnClick(R.id.test_input_validate_button)
-	void onClickValidate(View v) {
-		validate(answerView.getText());
-	}
+    @OnClick(R.id.test_input_validate_button)
+    void onClickValidate(View v) {
+        Editable answer = answerView.getText();
+        if (TextUtils.isEmpty(answer)) {
+            new AlertDialog.Builder(this)
+                    .setMessage(R.string.test_input_empty)
+                    .setPositiveButton(android.R.string.yes, (dialog, id) -> validate(answer))
+                    .setNegativeButton(android.R.string.no, null)
+                    .create()
+                    .show();
+        } else {
+            validate(answer);
+        }
+    }
 
-	@Override
-	protected void next(Item item) {
-		mScrollView.smoothScrollTo(0, 0);
-		answerView.setText(null);
+    @Override
+    protected void next(Item item) {
+        mScrollView.smoothScrollTo(0, 0);
+        answerView.setText(null);
 
-		String answer = null;
-		String toFind = null;
+        String answer = null;
+        String toFind = null;
 
-		switch (typeTest) {
+        switch (typeTest) {
 
-			case 0: // Kanji -> Hiragana
-				if (item.kanji.contains("、")) {
-					String[] kanjis = item.kanji.split("、");
-					toFind = kanjis[new Random().nextInt(2) - 1];
-				} else
-					toFind = item.kanji;
+            case 0: // Kanji -> Hiragana
+                if (item.kanji.contains("、")) {
+                    String[] kanjis = item.kanji.split("、");
+                    toFind = kanjis[new Random().nextInt(2) - 1];
+                } else
+                    toFind = item.kanji;
 
-				answer = item.kana;
-				break;
+                answer = item.kana;
+                break;
 
-			case 1: // Hiragana -> Kanji
-				toFind = item.kana;
-				answer = item.kanji;
-				break;
+            case 1: // Hiragana -> Kanji
+                toFind = item.kana;
+                answer = item.kanji;
+                break;
 
-			case 2: // Japanese -> French
-				answer = item.input;
+            case 2: // Japanese -> French
+                answer = item.input;
 
-				if (isDisplayKanji && StringUtils.isNoneBlank(item.kanji)) {
-					toFind = item.kanji;
-				} else {
-					toFind = item.kana;
-				}
-				break;
+                if (isDisplayKanji && StringUtils.isNoneBlank(item.kanji)) {
+                    toFind = item.kanji;
+                } else {
+                    toFind = item.kana;
+                }
+                break;
 
-			case 3: // French -> Japanese
-				toFind = item.input;
+            case 3: // French -> Japanese
+                toFind = item.input;
 
-				if (isDisplayKanji && StringUtils.isNoneBlank(item.kanji)) {
-					answer = item.kanji;
-				} else {
-					answer = item.kana;
-				}
-				break;
-		}
+                if (isDisplayKanji && StringUtils.isNoneBlank(item.kanji)) {
+                    answer = item.kanji;
+                } else {
+                    answer = item.kana;
+                }
+                break;
+        }
 
-		toFindView.setText(toFind);
-		results.add(new Result(toFind, answer, true));
-	}
+        toFindView.setText(toFind);
+        results.add(new Result(toFind, answer, true));
+    }
 
 }
