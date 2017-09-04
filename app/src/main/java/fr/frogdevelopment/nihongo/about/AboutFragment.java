682b89b3f4ff97457fb46679c8654c48cafc86eb
Benoît Le Gall <benoit.legall@bio-c-bon.fr>
2017-09-04 13:28:49 +0200

    add version to about view

--- app/src/main/java/fr/frogdevelopment/nihongo/about/AboutFragment.java
+++ app/src/main/java/fr/frogdevelopment/nihongo/about/AboutFragment.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import fr.frogdevelopment.nihongo.BuildConfig;
 import fr.frogdevelopment.nihongo.R;
 
 public class AboutFragment extends Fragment {
@@ -22,12 +23,15 @@ public class AboutFragment extends Fragment {
 		// Inflate the layout for this fragment
 		View rootView = inflater.inflate(R.layout.fragment_about, container, false);
 
+		TextView version = rootView.findViewById(R.id.about_version);
+		version.setText(getString(R.string.about_version, BuildConfig.VERSION_NAME));
+
 		MovementMethod instance = LinkMovementMethod.getInstance();
 
-		TextView hiragana = (TextView) rootView.findViewById(R.id.about_hiragana_link);
+		TextView hiragana = rootView.findViewById(R.id.about_hiragana_link);
 		hiragana.setMovementMethod(instance);
 
-		TextView katakana= (TextView) rootView.findViewById(R.id.about_katakana_link);
+		TextView katakana = rootView.findViewById(R.id.about_katakana_link);
 		katakana.setMovementMethod(instance);
 
 		return rootView;
