23da9851513551e2248729c7d75a908252c74916
LE GALL Benoît <legall.benoit@gmail.com>
2019-12-09 22:54:54 +0100

    fix: use title for fetching lesson

--- app/src/main/java/fr/frogdevelopment/nihongo/lessons/LessonsFragment.java
+++ app/src/main/java/fr/frogdevelopment/nihongo/lessons/LessonsFragment.java
@@ -42,6 +42,7 @@ import static android.net.ConnectivityManager.CONNECTIVITY_ACTION;
 import static android.view.View.VISIBLE;
 import static android.widget.RelativeLayout.LayoutParams.MATCH_PARENT;
 import static java.util.stream.Collectors.toList;
+import static org.apache.commons.lang3.StringUtils.uncapitalize;
 
 public class LessonsFragment extends ListFragment {
 
@@ -203,7 +204,7 @@ public class LessonsFragment extends ListFragment {
     }
 
     private void downloadLesson(Lesson lesson) {
-        mLessonsService.fetchLessons(mLocale, lesson.code).enqueue(new Callback<List<Details>>() {
+        mLessonsService.fetchLessons(mLocale, uncapitalize(lesson.title)).enqueue(new Callback<List<Details>>() {
             @Override
             public void onResponse(@NonNull Call<List<Details>> call, @NonNull Response<List<Details>> response) {
                 if (response.isSuccessful()) {
