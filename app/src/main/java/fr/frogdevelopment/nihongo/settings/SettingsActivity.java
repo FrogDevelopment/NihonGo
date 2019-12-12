3e254787c69348a83a7c38550d5c518d204ac471
Le Gall Benoît <benoit.legall@thanksys.com>
2019-12-12 16:56:14 +0100

    fix: AboutSettingsFragment not bundled

--- app/src/main/java/fr/frogdevelopment/nihongo/settings/SettingsActivity.java
+++ app/src/main/java/fr/frogdevelopment/nihongo/settings/SettingsActivity.java
@@ -4,20 +4,21 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import fr.frogdevelopment.nihongo.R;
 
-public class SettingsActivity extends AppCompatActivity implements
-        PreferenceFragmentCompat.OnPreferenceStartFragmentCallback {
+public class SettingsActivity extends AppCompatActivity implements PreferenceFragmentCompat.OnPreferenceStartFragmentCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
+            getSupportFragmentManager()
+                    .beginTransaction()
                     .replace(R.id.settings_container, new SettingsFragment())
                     .commitNow();
         }
@@ -26,12 +27,21 @@ public class SettingsActivity extends AppCompatActivity implements
 
     @Override
     public boolean onPreferenceStartFragment(PreferenceFragmentCompat caller, Preference pref) {
+        FragmentManager supportFragmentManager = getSupportFragmentManager();
+
         // Instantiate the new Fragment
-        final Fragment fragment = getSupportFragmentManager().getFragmentFactory().instantiate(getClassLoader(), pref.getFragment());
-        fragment.setArguments(pref.getExtras());
+        Fragment fragment;
+        if ("about".equals(pref.getKey())) {
+            fragment = new AboutSettingsFragment();
+        } else {
+            fragment = supportFragmentManager.getFragmentFactory().instantiate(getClassLoader(), pref.getFragment());
+        }
+        Bundle extras = pref.getExtras();
+        fragment.setArguments(extras);
         fragment.setTargetFragment(caller, 0);
-        // Replace the existing Fragment with the new Fragment
-        getSupportFragmentManager().beginTransaction()
+
+        supportFragmentManager
+                .beginTransaction()
                 .replace(R.id.settings_container, fragment)
                 .addToBackStack(null)
                 .commit();
